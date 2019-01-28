1856a446822987a45d1b2b92e75c7ac2da765f4f
Andrew Aluko <aluko17@Andrews-MacBook-Air-3.local>
2019-01-28 17:55:35 -0600

    users

---
+++ insert-users.sql
@@ -0,0 +1,7 @@
+INSERT INTO Users(user_id, first_name, last_name, email, zipcode)
+VALUES
+	(1, 'Andrew', 'Aluko', 'aluko17@icloud.com', 78702),
+	(2, 'Tristan', 'Bennett', 'tribe@yahoo.com', 78704),
+	(3, 'Josh', 'Leidich', 'jleid@aol.com', 78702),
+	(4, 'Lance', 'Schexnaydre', 'lschex@gmail.com', 78751),
+	(5, 'Christopher', 'Conley', 'ccon@yahoo.com', 78746)
