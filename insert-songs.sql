e3c806dc1b1f7cde72441762aef1e91c106936dd
Andrew Aluko <aluko17@Andrews-MacBook-Air-3.local>
2019-01-29 16:59:52 -0600

    insert new songs

--- insert-songs.sql
+++ insert-songs.sql
@@ -5,4 +5,11 @@ VALUES
 	(3, 3, 'Henny Dreams'),
 	(4, 4, 'Chanel'),
 	(5, 5, 'Maria Tambien')
+	(6, 2, 'The Sequence')
+	(7, 3, 'Communion')
+	(8, 4, 'Nikes')
 	
+-- -- 4. 
+-- 	a) insert 3 new songs for any artist 
+-- 	b) tie them to one user 
+-- 	c) write a query that returns each users most popular artist
